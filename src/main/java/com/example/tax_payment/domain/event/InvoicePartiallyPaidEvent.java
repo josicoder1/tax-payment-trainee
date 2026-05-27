f32aa92eea8510dda10e8289f7dc0930dff8e44a
Venusia <venusiabiruk16@gmail.com>
2026-05-27 05:22:57 -0700

    feat:create Invoice

--- src/main/java/com/example/tax_payment/domain/event/InvoicePartiallyPaidEvent.java
+++ src/main/java/com/example/tax_payment/domain/event/InvoicePartiallyPaidEvent.java
@@ -1,4 +1,10 @@
 package com.example.tax_payment.domain.event;
 
-public class InvoicePartiallyPaidEvent {
+import java.time.Instant;
+import java.util.UUID;
+
+public record InvoicePartiallyPaidEvent(
+        UUID invoiceId,
+        Instant occurredAt
+) implements DomainEvent {
 }
