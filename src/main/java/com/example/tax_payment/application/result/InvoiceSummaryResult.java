733c23b334ae3aba6b71c843a1f227675aa3419f
anansi <anansisime7@gmail.com>
2026-05-29 16:37:26 +0300

    feat-application: add results

--- src/main/java/com/example/tax_payment/application/result/InvoiceSummaryResult.java
+++ src/main/java/com/example/tax_payment/application/result/InvoiceSummaryResult.java
@@ -3,23 +3,12 @@ package com.example.tax_payment.application.result;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record InvoiceResult(
+public record InvoiceSummaryResult(
         UUID invoiceId,
-        String taxpayerTin,
         String taxType,
         String taxPeriod,
         String status,
-
-        BigDecimal principalAmount,
-        BigDecimal interestAmount,
-        BigDecimal penaltyAmount,
-
-        BigDecimal paidPrincipal,
-        BigDecimal paidInterest,
-        BigDecimal paidPenalty,
-
         BigDecimal outstandingAmount,
-
         String currency
 ) {
 }
