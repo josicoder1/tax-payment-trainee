5fa1652dd08d98023a6d3d3d477b8985008b8042
anansi <anansisime7@gmail.com>
2026-05-29 16:48:36 +0300

    add mapper

---
+++ src/main/java/com/example/tax_payment/application/mapper/InvoiceResultMapper.java
@@ -0,0 +1,43 @@
+package com.example.tax_payment.application.mapper;
+
+import com.example.tax_payment.application.result.InvoiceResult;
+import com.example.tax_payment.application.result.InvoiceSummaryResult;
+import com.example.tax_payment.domain.model.Invoice;
+
+public class InvoiceResultMapper {
+
+    public InvoiceResult toResult(Invoice invoice) {
+
+        return new InvoiceResult(
+                invoice.getId(),
+                invoice.getTaxpayerTin(),
+                invoice.getTaxType().getCode(),
+                invoice.getTaxPeriod().toString(),
+                invoice.getStatus().name(),
+
+                invoice.getPrincipalAmount().getAmount(),
+                invoice.getInterestAmount().getAmount(),
+                invoice.getPenaltyAmount().getAmount(),
+
+                invoice.getTotalPaidPrincipal().getAmount(),
+                invoice.getTotalPaidInterest().getAmount(),
+                invoice.getTotalPaidPenalty().getAmount(),
+
+                invoice.getTotalOutstanding().getAmount(),
+
+                invoice.getCurrency()
+        );
+    }
+
+    public InvoiceSummaryResult toSummaryResult(Invoice invoice) {
+
+        return new InvoiceSummaryResult(
+                invoice.getId(),
+                invoice.getTaxType().getCode(),
+                invoice.getTaxPeriod().toString(),
+                invoice.getStatus().name(),
+                invoice.getTotalOutstanding().getAmount(),
+                invoice.getCurrency()
+        );
+    }
+}
