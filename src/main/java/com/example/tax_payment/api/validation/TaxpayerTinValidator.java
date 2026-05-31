a95665287dc5c2a6ca970f16ffa6eeaf3c7649aa
anansi <anansisime7@gmail.com>
2026-05-31 15:21:10 +0300

    feat: add exeption and validation

--- src/main/java/com/example/tax_payment/api/validation/TaxpayerTinValidator.java
+++ src/main/java/com/example/tax_payment/api/validation/TaxpayerTinValidator.java
@@ -1,4 +1,15 @@
 package com.example.tax_payment.api.validation;
 
 public class TaxpayerTinValidator {
+
+    public static void validate(String tin) {
+
+        if (tin == null || tin.isBlank()) {
+            throw new IllegalArgumentException("TIN cannot be empty");
+        }
+
+        if (!tin.matches("[0-9A-Z]{8,15}")) {
+            throw new IllegalArgumentException("Invalid TIN format");
+        }
+    }
 }
